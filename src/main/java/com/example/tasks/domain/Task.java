d422c69247205f477893c66e517c4e6aceece31d
Jonas Capelier <capelier.jonas@gmail.com>
2026-02-16 11:34:39 +0100

    Initial commit

---
+++ src/main/java/com/example/tasks/domain/Task.java
@@ -0,0 +1,12 @@
+package com.example.tasks.domain;
+
+public class Task {
+  private Long id;
+  private String title;
+  private boolean status;
+
+  public Task (String name) {
+    this.name = name;
+    id =
+  }
+}
